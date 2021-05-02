61223a1f8d8cccb11fe7a5f26ed7d793da34cfda
Martin Wanczyk <wanczyk.martin@gmail.com>
2021-05-02 20:23:36 +0200

    Add cover field to book model

--- bookShop/settings.py
+++ bookShop/settings.py
@@ -160,6 +160,9 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
